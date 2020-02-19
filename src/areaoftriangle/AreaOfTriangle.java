bd84b14f2fb64cec2e34958d257d58967c5a6fa2
antho6229 <antho6229@7L0M6Y1.Student.UGDSB.ED>
2020-02-19 13:57:50 -0500

    Added header

--- src/areaoftriangle/AreaOfTriangle.java
+++ src/areaoftriangle/AreaOfTriangle.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Andrew Thompson
+ * February 19, 2020
+ * Calculates the area of a triangle
  */
 
 package areaoftriangle;
